184ea9567be3fd535d5cc1a112a79c732f991843
Matt Wetmore <wetmore.matt@gmail.com>
2013-11-09 10:16:55 -0500

    make it pretty n shit

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,6 +1,9 @@
 Meteor.startup(function() {
-
-  if (Topics.find().count() === 0) {
+  //if (Topics.find().count() === 0) {
+    Topics.remove({});
+    Sections.remove({});
+    Definitions.remove({});
+    Propositions.remove({});
     var topic_id = Topics.insert({
       name: 'Graph Theory',
       encodedname: 'graphtheory'
@@ -10,13 +13,57 @@ Meteor.startup(function() {
         name: 'Basic Definitions',
         number: 1,
         topic: topic_id,
+        description: 'Important terms, the handshake lemma',
         encodedname: 'basicdefinitions'
       },
       {
         name: 'Connectivity',
         number: 2,
         topic: topic_id,
+        description: 'Connected components, cut edges',
         encodedname: 'connectivity'
+      },
+      {
+        name: 'Trees and Forests',
+        number: 3,
+        topic: topic_id,
+        description: 'Acyclic simple graphs',
+        encodedname: 'treesandforests'
+      },
+      {
+        name: 'Spanning Trees',
+        number: 4,
+        topic: topic_id,
+        description: 'Fundamental cycles, minimum-cost trees',
+        encodedname: 'spanningtrees'
+      },
+      {
+        name: 'Shortest Paths',
+        number: 5,
+        topic: topic_id,
+        description: 'Shortest-path trees, Dijkstra\'s algorithm',
+        encodedname: 'shortestpaths'
+      },
+      {
+        name: 'Euler\'s Theorem and Hamiltonian Paths',
+        number: 6,
+        topic: topic_id,
+        description: 'Necessary/sufficient conditions',
+        encodedname: 'eulerhamiltonian'
+      },
+      {
+        name: 'Bipartite Graphs',
+        number: 7,
+        topic: topic_id,
+        description: 'Bipartitions, necessary conditions',
+        encodedname: 'bipartite'
+      },
+      {
+        name: 'Matchings in Bipartite Graphs',
+        number: 8,
+        topic: topic_id,
+        description: 'König\'s theorem, vertex covers, Hall\'s theorem',
+        encodedname: 'bipartitematchings'
       }
     ];
     var section_ids = [];
@@ -27,7 +74,7 @@ Meteor.startup(function() {
     var definitions = [
       {
         term: 'Graph',
-        body: 'A {graph} $G$ is an ordered pair $(V(G), E(G))$, where $V(G)$ is a set of {vertices}, $E(G)$ is a set of {edges}, and a edge is said to be {incident} to one or two vertices, called its {ends}. If $e$ is incident to vertices $u$ and $v$, we write $e = uv = vu$.',
+        body: 'A graph $G$ is an ordered pair $(V(G), E(G))$, where $V(G)$ is a set of vertices, $E(G)$ is a set of edges, and a edge is said to be incident to one or two vertices, called its ends. If $e$ is incident to vertices $u$ and $v$, we write $e = uv = vu$.',
         section: section_ids[0]
       },
       {
@@ -43,15 +90,26 @@ Meteor.startup(function() {
       {
         type: 'lemma',
         section: section_ids[1],
+        number: 1,
+        statement: '',
+        proof: 'Trivial'
+      }, {
+        type: 'lemma',
+        section: section_ids[1],
+        number: 2,
+        statement: '',
+        proof: 'Trivial'
+      }, {
+        type: 'lemma',
+        section: section_ids[1],
         number: 3,
         statement: 'If there is a walk with ends $u, v$ in $G$, then there is a path in $G$ with the same ends.',
-        proof: 'Choose the walk in $G$ with ends $u$ and $v$ of minimal length. We will show that it corresponds to a path, i.e. that it has no repeated vertices. \
-        Suppose not, i.e. the walk is given by $v_0 e_1 v_1 \\ldots v_n$, and $v_i = v_j$ for some $i < j$. Then $v_0 e_1 v_1 \\ldots v_i v_{j+1} \\ldots v_n$ is a shorter walk with the same ends, which proves the claim by contradiction.'
+        proof: 'Choose the walk in $G$ with ends $u$ and $v$ of minimal length. We will show that it corresponds to a path, i.e. that it has no repeated vertices. Suppose not, i.e. the walk is given by $v_0 e_1 v_1 \\cdots v_n$, and $v_i = v_j$ for some $i < j$. Then $v_0 e_1 v_1 \\cdots v_i v_{j+1} \\cdots v_n$ is a shorter walk with the same ends, which proves the claim by contradiction.'
       }
     ];
     for (var i = 0; i < props.length; i++) {
       Propositions.insert(props[i]);
     };
 
-  }
+//  }
 });
