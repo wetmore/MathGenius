6250f0768e47af12c8accf5ff785851c8ad2a747
Sami Jaber <jabersami@gmail.com>
2013-11-09 11:28:10 -0500

    fixed missing THEFUNCTION.js

--- THEFUNCTION.js
+++ THEFUNCTION.js
@@ -16,6 +16,8 @@ function getSelectedText() {
 
 		var mathjax = parseInt(temp.getAttribute('id'));
 
+		console.log(mathjax * 2, mathjax * 2 + 1);
+			
 		return {
 			dollar1: mathjax * 2,
 			dollar2: mathjax * 2 + 1,
@@ -23,9 +25,42 @@ function getSelectedText() {
 			offset2:0
 		};
 	}
-	else {
-		var offset =
+	else 
+	{
+		var offset1 = sln.anchorOffset;
+		var offset2 = sln.focusOffset;
+		var par = document.getElementsByClassName('prop-proof')[0];
 
+		var dllr1 = 0;
+	    var temp = par.firstChild;
+
+		while(temp != node && temp != null) {
+			if (temp.nodeName == "SCRIPT") {
+				dllr1 ++;
+			}
+			temp = temp.nextSibling;
+    	}
+
+    	//same thing but for second index
+    	var node2 = sln.focusNode;
+		temp = par.firstChild;
+		var dllr2 = 0;
+
+		while(temp != node2 && temp != null) {
+			if (temp.nodeName == "SCRIPT") {
+				dllr2 ++;
+			}
+			temp = temp.nextSibling;
+    	}
+
+    	console.log(dllr1 * 2, dllr2 * 2, offset1, offset2);
+    	
+    	return {
+			dollar1: dllr1 * 2,
+			dollar2: dllr2 * 2,
+			offset1: offset1,
+			offset2: offset2
+		};
 	}
 };
 
