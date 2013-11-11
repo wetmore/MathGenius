c35f73b62accf303ac3ab87b5bf585d1d0c29696
Matt Wetmore <wetmore.matt@gmail.com>
2013-11-11 14:04:26 -0500

    more data

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -79,10 +79,26 @@ Meteor.startup(function() {
         section: section_ids[0]
       },
       {
+        term: 'Loop',
+        body: 'A loop is an edge with only one end',
+        section: section_ids[0]
+      },
+      {
+        term: 'Parallel',
+        body: 'Edges are said to be parallel if they are incident to the same two vertices.',
+        section: section_ids[0]
+      },
+      {
         term: 'Connected',
         body: 'A graph $G$ is connected if and only if for all $u, v \\in V(G)$, there exists a walk from $u$ to $v$',
         section: section_ids[1]
+      },
+      {
+        term: 'Componenents',
+        body: 'A connected component of a graph $G$ is a maximal connected subgraph.',
+        section: section_ids[1]
       }
+        
     ];
     for (var i = 0; i < definitions.length; i++) {
       Definitions.insert(definitions[i]);
@@ -92,20 +108,26 @@ Meteor.startup(function() {
         type: 'lemma',
         section: section_ids[1],
         number: 1,
-        statement: '',
-        proof: 'Trivial'
+        statement: 'Let $G$ be a graph which is not connected. Then there exists a partition $(X, Y)$ of $G$, $X, Y \\neq \\emptyset$, such that no edge of $G$ has one end in $X$ and another in $Y$.',
+        proof: '<span class="line-sep" id="1">Trivial</span>'
       }, {
         type: 'lemma',
         section: section_ids[1],
         number: 2,
-        statement: '',
-        proof: 'Trivial'
+        statement: 'Let $G$ be a connected graph, let $X \\subsetneq V(G)$ with $X \\neq \\emptyset$. Then there exists an edge of $G$ with one edge in $X$ and another in $V(G) - X$.',
+        proof: '<span class="line-sep" id="1">Trivial</span>'
       }, {
         type: 'lemma',
         section: section_ids[1],
         number: 3,
         statement: 'If there is a walk with ends $u, v$ in $G$, then there is a path in $G$ with the same ends.',
         proof: '<span class="line-sep" id="1">Choose the walk in $G$ with ends $u$ and $v$ of minimal length. </span><span class="line-sep" id="2">We will show that it corresponds to a path, i.e. that it has no repeated vertices. </span><span class="line-sep" id="3">Suppose not, i.e. the walk is given by $v_0 e_1 v_1 \\cdots v_n$, and $v_i = v_j$ for some $i < j$. </span><span class="line-sep" id="4">Then $v_0 e_1 v_1 \\cdots v_i v_{j+1} \\cdots v_n$ is a shorter walk with the same ends, which proves the claim by contradiction.</span>'
+      }, {
+        type: 'lemma',
+        section: section_ids[1],
+        number: 4,
+        statement: 'If $H_1, H_2$ are connected subgraphs of $G$ so that $H_1 \\cap H_2 \\neq \\emptyset$, then $H_1 \\cup H_2$ is connected.',
+        proof: '<span class="line-sep" id="1">Let $w \\in V(H_1) \\cap V(H_2)$.</span><span class="line-sep" id="2"> Now, for all $u, v \\in V(H_1 \\cup H_2) = V(H_1) \\cup V(H_2)$, there exists a walk from $u$ to $w$ which lies entirely WLOG in $H_1$,</span><span class="line-sep" id="3"> and a walk from $w$ to $v$ lying WLOG entirely in $H_2$.</span><span class="line-sep" id="4"> Concatenating these walks, the result is immediate.</span>'
       }
     ];
     for (var i = 0; i < props.length; i++) {
